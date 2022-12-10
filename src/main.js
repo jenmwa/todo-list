624621e220a225b99d017b37449f5ab008411c29
jenmwa <jenmwa@gmail.com>
2022-12-10 14:27:19 +0100

    :construction: task object in the makin-

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ const taskList = ['pepparkaksDeg', 'ringUnni'];
 
 const tasks = document.querySelector('.tasks'); // the <ul> element
 
-printTaskList()
+//printTaskList()
 
 // Todays Date
 const todaysDate = new Date();
@@ -23,58 +23,84 @@ const dateField = document.querySelector('#todaysDate');
 dateField.innerHTML = todaysDate.toLocaleDateString();
 
 const submitBtn = document.querySelector('#submit'); //submitBtn
-submitBtn.addEventListener('click', addNewTask);
+submitBtn.addEventListener('click', /*addNewTask*/ storeValues);
 
 const newTaskInput = document.querySelector('#inputTaskField');
+const deadLineInput = document.querySelector('#deadlineInput');
 
-function addNewTask(e) {
+function storeValues(e) {
     e.preventDefault();
-    if (newTaskInput.value.length === 0) {
-      return;
+    // localStorage.setItem('userTasks', JSON.stringify(userTasks));
+    const input = {
+      task: newTaskInput.value,
+      deadline: deadLineInput.value,
+      addedDate: new Date(),
+      category: '1,2,3 eller 4',
+      isComplete: false,
     }
-    if (taskList.indexOf(newTaskInput.value) === -1) {
-      taskList.push(newTaskInput.value);
-      printTaskList();
-    }
-}
+    localStorage.setItem('userTasks', JSON.stringify(input));
+    printHTML();
+  }
 
-function printTaskList() {
-    tasks.innerHTML='';
+  function getValues() {
+    return localStorage.getItem('userTasks');
+  }
+  
+  function printHTML() {
+    console.log(JSON.parse(localStorage.getItem('userTasks')));
+    const task = getValues();
+    tasks.innerHTML = task; 
+  }
 
-    for (let i = 0; i < taskList.length; i++) {
-        const taskName = taskList[i];
-        //const taskName = taskList[i].content + ' (' + taskList[i].deadline + ')';
-        const taskNode = document.createElement('li');
-        const taskTextNode = document.createTextNode(taskName);
+// function addNewTask(e) {
+//     e.preventDefault();
+//     if (newTaskInput.value.length === 0) {
+//       return;
+//     }
+//     if (taskList.indexOf(newTaskInput.value) === -1) {
+//       taskList.push(newTaskInput.value);
+//       printTaskList();
+//       console.log(taskList);
+//     }
+// }
 
-        //trash
-        const trashIcon = document.createElement('button');
-        trashIcon.setAttribute('data-name', taskName);
-        trashIcon.classList.add('material-symbols-outlined');
-        const trashIconText = document.createTextNode ('delete');
-        trashIcon.appendChild(trashIconText);
+// function printTaskList() {
+//     tasks.innerHTML='';
 
-        taskNode.appendChild(taskTextNode);
-        taskNode.appendChild(trashIcon);
+//     for (let i = 0; i < taskList.length; i++) {
+//         const taskName = taskList[i];
+//         //const taskName = taskList[i].content + ' (' + taskList[i].deadline + ')';
+//         const taskNode = document.createElement('li');
+//         const taskTextNode = document.createTextNode(taskName);
 
-        tasks.appendChild(taskNode); 
-    }
+//         //trash
+//         const trashIcon = document.createElement('button');
+//         trashIcon.setAttribute('data-name', taskName);
+//         trashIcon.classList.add('material-symbols-outlined');
+//         const trashIconText = document.createTextNode ('delete');
+//         trashIcon.appendChild(trashIconText);
 
-    const taskItems = Array.from(document.querySelectorAll('li button'));
-    taskItems.forEach((item) => {
-        item.addEventListener('click', removeTask)
-    });
-}
-/**
- * Remove tasks taskList
- */
-function removeTask(e) {
-    const index = taskList.indexOf(e.target.dataset.name); 
-    if (index > -1) {
-        taskList.splice(index, 1);
-        printTaskList();
-    }
-}
+//         taskNode.appendChild(taskTextNode);
+//         taskNode.appendChild(trashIcon);
+
+//         tasks.appendChild(taskNode); 
+//     }
+
+//     const taskItems = Array.from(document.querySelectorAll('li button'));
+//     taskItems.forEach((item) => {
+//         item.addEventListener('click', removeTask)
+//     });
+// }
+// /**
+//  * Remove tasks taskList
+//  */
+// function removeTask(e) {
+//     const index = taskList.indexOf(e.target.dataset.name); 
+//     if (index > -1) {
+//         taskList.splice(index, 1);
+//         printTaskList();
+//     }
+// }
 
 // const userTask = {
 //     task: 'input value',
