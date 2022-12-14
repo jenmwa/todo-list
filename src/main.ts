8293fa7a363cbb7131c4c8bb98b1cbec398e2511
jenmwa <jenmwa@gmail.com>
2022-12-14 21:11:29 +0100

    .

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import './style/style.scss';
-// ul = id=list
-// form id new-task-form
-// input text = new-task-title
-// button type submit
-
-const tasks = document.querySelector<HTMLUListElement>('#tasks');
-const app = document.querySelector<HTMLFormElement>('#app');
-const newTaskInput = document.querySelector<HTMLInputElement>('#newTaskInput');
