8a07c1a4e8ee02014826aef8d252764813c4accf
jenmwa <jenmwa@gmail.com>
2022-12-17 16:33:32 +0100

    :lipstick: category icons

--- src/main.js
+++ src/main.js
@@ -26,11 +26,11 @@ dateField.innerHTML = todaysDate.toLocaleDateString();
 // funktion lägg till ny todo som objekt till array
 function addNewTask() {  
   if (newTaskInput.value.length === 0 || deadlineInput.value === '') {
-    taskError.innerHTML = 'Fyll i todo & deadline!';
+    taskError.innerHTML = 'Fyll i Todo & deadline!';
     return;
   }
   
-  if (newTaskInput.value.length >= 2 && deadlineInput != '') {
+  else if (newTaskInput.value.length >= 2 && deadlineInput != '') {
     console.log(taskList.indexOf);
 
     taskError.innerHTML = '';
@@ -62,7 +62,7 @@ function printTaskList(taskList) {
         <input type="checkbox" name="checkbox" class="checkbox"data-id="${i}">
         <span class="text" id="texttodo">${taskList[i].task}</span><br>
         <span class="text"> ${taskList[i].deadline}</span></div><div class="rightsection">
-        <span class="material-symbols-outlined" id="favorite">${taskList[i].category}</span>
+        <span class="material-symbols-outlined category">${taskList[i].category}</span>
         <button class="material-symbols-outlined" data-id="${i}">close</button>
         </div>
         </li>`;
@@ -79,7 +79,7 @@ function printTaskList(taskList) {
   //klick-event för checkbox-knappen som anropar funktion checkedBox
   const checkBtn = Array.from(document.querySelectorAll('.tasks input'));
   checkBtn.forEach(check => {
-    check.addEventListener('click', todoChecked);
+    check.addEventListener('change', todoChecked);
   });
 }
  // NEDAN - work In progress!
@@ -100,13 +100,14 @@ function printTaskList(taskList) {
 //funktion när todo är checked, gråa ut text
 function todoChecked(event) {
   const index = taskList.findIndex(task => task.task === event.currentTarget.nextElementSibling.innerHTML);
-  console.log(event.currentTarget.nextElementSibling.innerHTML);
+  console.log(event.currentTarget.nextElementSibling);
 
   // if (taskList[index].isComplete  === false) {
   //   event.currentTarget.parentElement.classList.remove('checked');
   // }
   if (taskList[index].isComplete  === true) {
-    event.currentTarget.parentElement.classList.add('checked');
+    event.currentTarget.nextElementSibling.classList.add('checked');
+    console.log(event.currentTarget);
   
   }
     //KOD?
@@ -115,7 +116,7 @@ function todoChecked(event) {
   
   //addToLocalStorage(taskList); //OM denna syns här, så går det ej att klicka i checkbox! why?
 
-  console.log('click', event.currentTarget.dataset.id); //jag vill gråa ut symbolen för just denna li med + ändra bakgrund? hur?
+  //console.log('click', event.currentTarget.dataset.id); //jag vill gråa ut symbolen för just denna li med + ändra bakgrund? hur?
   //console.log(taskList);
 }
 
@@ -142,30 +143,30 @@ function removeTask(e) {
 /***** sorteraSektion OBS NÄR ALLT FÖR G ÄR KLART - REFAKTORERA KODEN! *****/
 // se Aritmetik v3 14 modul
 
-let isDateSort = true;
+//let isDateSort = true;
 let isNameSort = true;
 let isDeadlineSort = true;
 
-const SortByDateBtn = document.querySelector('#sortByDateBtn');
+//const SortByDateBtn = document.querySelector('#sortByDateBtn');
 const sortByNameBtn = document.querySelector('#sortByNameBtn');
 const sortByDeadlineBtn = document.querySelector('#sortByDeadlineBtn');
 
 //sortera per inlagt datum
-function sortByDate(eve) {
-  console.log('clicketi');
-  console.log(taskList);
-  eve.preventDefault();
-  if (isDateSort) {
-    taskList.sort((a, b) => a.addedDate.localeCompare(b.addedDate));
-    isDateSort = false;
-  } else if (isDateSort === false) {
-    taskList.sort((a, b) => b.addedDate.localeCompare(a.addedDate));
-    isDateSort = true;
-  }
-  printTaskList(taskList);
-  addToLocalStorage(taskList);
+// function sortByDate(eve) {
+//   console.log('clicketi');
+//   console.log(taskList);
+//   eve.preventDefault();
+//   if (isDateSort) {
+//     taskList.sort((a, b) => a.addedDate.localeCompare(b.addedDate));
+//     isDateSort = false;
+//   } else if (isDateSort === false) {
+//     taskList.sort((a, b) => b.addedDate.localeCompare(a.addedDate));
+//     isDateSort = true;
+//   }
+//   printTaskList(taskList);
+//   addToLocalStorage(taskList);
 
-}
+// }
 
 // Sortera per namn
 function sortByName(ev) {
@@ -206,7 +207,7 @@ function sortByComplete() {
   console.log(taskList);
 }
 
-SortByDateBtn.addEventListener('click', sortByDate);
+//SortByDateBtn.addEventListener('click', sortByDate);
 sortByNameBtn.addEventListener('click', sortByName);
 sortByDeadlineBtn.addEventListener('click', sortByDeadline);
 
