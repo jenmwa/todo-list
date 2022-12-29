734bfb815341ead9ff63bcde6399b77435804410
jenmwa <jenmwa@gmail.com>
2022-12-29 18:22:01 +0100

    :art: format code

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import './style/style.scss';
 
 let taskList = [];
 
-const tasks = document.querySelector('.tasks'); 
+const tasks = document.querySelector('.tasks');
 
-const newTaskInput = document.querySelector('#inputTaskField'); 
-const deadlineInput = document.querySelector('#deadlineInput'); 
+const newTaskInput = document.querySelector('#inputTaskField');
+const deadlineInput = document.querySelector('#deadlineInput');
 const submitBtn = document.querySelector('#submit');
-const resetBtn = document.querySelector('#resetAll')
+const resetBtn = document.querySelector('#resetAll');
 
 const sortSection = document.querySelector('#sortSection');
 
@@ -21,14 +21,13 @@ const dateField = document.querySelector('#todaysDate');
 dateField.innerHTML = todaysDate.toLocaleDateString();
 todaysDate = document.querySelector('#deadlineInput').valueAsDate;
 
-
 /************************************************************************************************************
  * -------------------------------------------  Functions  -------------------------------------------------
  ************************************************************************************************************/
 
 function getFromLocalStorage() {
   const getStoredArray = localStorage.getItem('taskList');
-  
+
   if (getStoredArray) {
     taskList = JSON.parse(getStoredArray);
     printTaskList(taskList);
@@ -36,16 +35,16 @@ function getFromLocalStorage() {
 }
 
 function setToLocalStorage() {
-  localStorage.setItem("taskList", JSON.stringify(taskList));
+  localStorage.setItem('taskList', JSON.stringify(taskList));
 }
 
-function addNewTask(e) { 
-  e.preventDefault(); 
+function addNewTask(e) {
+  e.preventDefault();
   if (newTaskInput.value.length === 0 || deadlineInput.value === '') {
     taskError.innerHTML = 'Fyll i Todo & deadline!';
     return;
   }
-  
+
   if (newTaskInput.value.length >= 2 && deadlineInput != '') {
     taskError.innerHTML = '';
     const selectedCategory = document.querySelector("input[name='category']:checked").value;
@@ -70,11 +69,11 @@ function printTaskList(taskList) {
   tasks.innerHTML = '';
 
   for (let i = 0; i < taskList.length; i++) {
-    const checkBox = taskList[i].isComplete ? 'todochecked' : ''; 
+    const checkBox = taskList[i].isComplete ? 'todochecked' : '';
     const toggled = taskList[i].isComplete ? 'toggled' : '';
     const checked = taskList[i].isComplete ? 'checked' : '';
     // const passed = taskList[i].deadline ? 'passed-deadline' : '';
-    // const soon = taskList[i].deadline ? 'deadline-in-5' : ''; 
+    // const soon = taskList[i].deadline ? 'deadline-in-5' : '';
     //console.log(taskList[i].deadline);
 
     // const checkBox;
@@ -100,29 +99,24 @@ function printTaskList(taskList) {
             <button class="material-symbols-outlined" aria-label="ta bort todo" data-id="${i}">close</button>
           </div>
           </li>`;
-        
-          
-          
-          function checkDeadlines () {
-          const deadlines = new Date(taskList[i].deadline);
-          const deadlineIn5days = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate() + 5);
-            
-          if (deadlines < todaysDate) {
-            console.log(taskList[i].deadline + ' deadline har passerat');
-            //classList.add('passed-deadline');
-            
-          }
-          else if (deadlines <= deadlineIn5days) {
-            console.log(taskList[i].deadline + ' deadline är inom 5 dagar');
-            //classList.add('.deadline-in-5');
-            
-          }
-        }
+
+    function checkDeadlines() {
+      const deadlines = new Date(taskList[i].deadline);
+      const deadlineIn5days = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate() + 5);
+
+      if (deadlines < todaysDate) {
+        console.log(taskList[i].deadline + ' deadline har passerat');
+        //classList.add('passed-deadline');
+      } else if (deadlines <= deadlineIn5days) {
+        console.log(taskList[i].deadline + ' deadline är inom 5 dagar');
+        //classList.add('.deadline-in-5');
+      }
     }
-    showsortSection();
-    todoEventListeners();
   }
- 
+  showsortSection();
+  todoEventListeners();
+}
+
 function todoEventListeners() {
   const taskItems = Array.from(document.querySelectorAll('.tasks button'));
   taskItems.forEach(item => {
@@ -132,12 +126,12 @@ function todoEventListeners() {
   const checkBtn = Array.from(document.querySelectorAll('.tasks input'));
   checkBtn.forEach(check => {
     check.addEventListener('change', todoChecked);
-  });  
+  });
 }
 
 function todoChecked(event) {
   const todo = taskList[event.currentTarget.dataset.id];
-  console.log(taskList[event.currentTarget.dataset.id])
+  console.log(taskList[event.currentTarget.dataset.id]);
 
   if (event.target.checked) {
     todo.isComplete = true;
@@ -154,11 +148,10 @@ function todoChecked(event) {
   sortByComplete();
 }
 
-function showsortSection() { 
+function showsortSection() {
   if (taskList.length <= 1) {
     sortSection.classList.remove('open');
     document.querySelector('#sortByText').innerHTML = ' ';
-
   }
   if (taskList.length >= 2) {
     sortSection.classList.add('open');
@@ -185,20 +178,20 @@ const sortByNameBtn = document.querySelector('#sortByNameBtn');
 const sortByDeadlineBtn = document.querySelector('#sortByDeadlineBtn');
 
 function sortByDate(e) {
-e.preventDefault();
-document.querySelector('#sortByText').innerHTML = 'inlagt datum';
-const sorted = taskList.sort((a, b) => {
-  if (a.addedDate < b.addedDate) {
-    return -1;
-  }
-  if (a.addedDate > b.addedDate) {
-    return 1;
-  }
-  return 0;
-});
+  e.preventDefault();
+  document.querySelector('#sortByText').innerHTML = 'inlagt datum';
+  const sorted = taskList.sort((a, b) => {
+    if (a.addedDate < b.addedDate) {
+      return -1;
+    }
+    if (a.addedDate > b.addedDate) {
+      return 1;
+    }
+    return 0;
+  });
 
-setToLocalStorage();
-printTaskList(taskList);
+  setToLocalStorage();
+  printTaskList(taskList);
 }
 
 function sortByName(ev) {
@@ -233,16 +226,15 @@ function sortByComplete() {
   const done = taskList.sort((a, b) => {
     if (a.isComplete < b.isComplete) {
       return -1;
-  
     }
     if (a.isComplete > b.isComplete) {
       return 1;
     }
     return 0;
   });
-  setToLocalStorage()
+  setToLocalStorage();
   printTaskList(taskList);
-  }
+}
 
 SortByDateBtn.addEventListener('click', sortByDate);
 sortByNameBtn.addEventListener('click', sortByName);
@@ -264,17 +256,17 @@ function resetAll(event) {
  * ------------------------------------  Light/DarkMode Toggle ---------------------------------------------
  ************************************************************************************************************/
 
-const colorTheme = document.querySelector("body");
-const colorModeIcon = document.querySelector(".light-mode-icon");
+const colorTheme = document.querySelector('body');
+const colorModeIcon = document.querySelector('.light-mode-icon');
 
 function toggleColorMode() {
   colorTheme.classList.toggle('change');
   if (colorTheme.classList.contains('change')) {
-    colorModeIcon.textContent = "dark_mode";
+    colorModeIcon.textContent = 'dark_mode';
   } else {
-    colorModeIcon.textContent = "light_mode";
+    colorModeIcon.textContent = 'light_mode';
   }
-};
+}
 
 /************************************************************************************************************
  * ------------------------------------  EventListeners -----------------------------------------------------
@@ -288,6 +280,6 @@ colorModeIcon.addEventListener('click', toggleColorMode);
 
 /************************************************************************************************************
  * ------------------------------------  get from local Storage ---------------------------------------------
- ************************************************************************************************************/
+ ******************************************* last at doc ****************************************************/
 
 getFromLocalStorage();
