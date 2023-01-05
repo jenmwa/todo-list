8ae71bd5473ad26ada946a1eb44840f70b5f3dd9
jenmwa <jenmwa@gmail.com>
2023-01-05 15:44:27 +0100

    push

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ då ska "base" här nedan vara "/fed22d-js-grundkurs-jenni-wumpus/"
  */
 
 export default defineConfig({
-  base: '/jenmwa/todo/', 
+  base: '/todo/', 
   define: {
     'process.env': {},
   },
